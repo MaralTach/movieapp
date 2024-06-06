52971692c2a778743ee93309ef74861e22b6f977
MaralTach <yoloten.maral@gmail.com>
2024-06-07 01:04:02 +0300

    add register

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,12 +12,16 @@ import {
   Transition,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar() {
+  const currentUser ={
+    displayName:"Maral"
+  }
   const [showBackground, setShowBackground] = useState(false);
 
   useEffect(() => {
@@ -46,9 +50,12 @@ export default function Navbar() {
         }`}
       >
         <div className="relative flex h-16 items-center justify-between">
+          <Link href="/">
           <img className="h-4 lg:h-7" src="/images/logo.png" alt="logo" />
+          </Link>
 
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {currentUser && <h5>{currentUser?.displayName}</h5>}
             {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
               <div>
@@ -57,8 +64,9 @@ export default function Navbar() {
                   <span className="sr-only">Open user menu</span>
                   <img
                     className="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
+                    src={currentUser?.photoURL || "/images/default-slate.png"}
+                    alt="user"
+                    referrerPolicy="no-referrer"
                   />
                 </MenuButton>
               </div>
@@ -73,41 +81,54 @@ export default function Navbar() {
                 <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                   <MenuItem>
                     {({ focus }) => (
-                      <a
-                        href="#"
+                      <Link
+                        href="/register"
                         className={classNames(
                           focus ? "bg-gray-100" : "",
                           "block px-4 py-2 text-sm text-gray-700"
                         )}
                       >
-                        Your Profile
-                      </a>
+                        Register
+                      </Link>
                     )}
                   </MenuItem>
                   <MenuItem>
                     {({ focus }) => (
-                      <a
-                        href="#"
+                      <Link
+                        href="/login"
                         className={classNames(
                           focus ? "bg-gray-100" : "",
                           "block px-4 py-2 text-sm text-gray-700"
                         )}
                       >
-                        Settings
-                      </a>
+                        Login
+                      </Link>
                     )}
                   </MenuItem>
                   <MenuItem>
                     {({ focus }) => (
-                      <a
-                        href="#"
+                      <Link
+                        href="/profile"
                         className={classNames(
                           focus ? "bg-gray-100" : "",
                           "block px-4 py-2 text-sm text-gray-700"
                         )}
                       >
-                        Sign out
-                      </a>
+                        Profile
+                      </Link>
+                    )}
+                  </MenuItem>
+                  <MenuItem>
+                    {({ focus }) => (
+                      <span
+                       
+                        className={classNames(
+                          focus ? "bg-gray-100" : "",
+                          "block px-4 py-2 text-sm text-gray-700 cursor-pointer"
+                        )}
+                      >
+                        Log out
+                      </span>
                     )}
                   </MenuItem>
                 </MenuItems>
